925dedaf03cd63eddaefaa0e344a321b516ec6bb
Nongoogle <essamsleem9@gmail.com>
2026-09-12 20:33:18 +0300

    Question01 B

--- Program.cs
+++ Program.cs
@@ -14,10 +14,15 @@
 
         static void Main(string[] args)
         {
-            #region Question1
+            #region Question1A
             //a)What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
 
             //the delevery address is struct so the copy we modifying does not affect the original
+            #endregion
+            #region Question1B
+            //b) What happens when a Customer variable is copied into another variable and one variable modifies the object
+
+            //the customer is class so when the copy modified it will make change in the other variable 
             #endregion
 
         }
