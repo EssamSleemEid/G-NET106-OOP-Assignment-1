4be628e03dfe50929e5701313248ce4092278bc5
Nongoogle <essamsleem9@gmail.com>
2026-09-12 20:30:18 +0300

    Question01 A

--- Program.cs
+++ Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        public struct DeliveryAddress
+        {
+            public string City; public string Street;
+        }
+
+        public class Customer
+        {
+            public string Name;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+            //a)What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+
+            //the delevery address is struct so the copy we modifying does not affect the original
+            #endregion
+
         }
     }
 }
